4ed533970bcc6e5c550a41fe77a552d7279fa8b8
Jeff Kerpan <jkerpan@gmail.com>
2017-05-05 18:21:49 -0600

    MVP #1 Working

--- main.js
+++ main.js
@@ -1,36 +1,78 @@
 $(document).ready(function() {
 
-
-
-  var dataArr = [];
+  // var dataArr = [];
 
   // Pulling earthquake data from beg. of year to 4/28/17 with a certain magnitude
-  var $xhr =
-  $.getJSON('https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2017-01-01&endtime=2017-04-28&minmagnitude=6.5');
+  // var $xhr =
+  // $.getJSON('https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2017-01-01&endtime=2017-04-28&minmagnitude=6.5');
+
+var $body = $('Body');
+
+  function getData(event) {
+  event.preventDefault();
+  console.log('PASS');
+  var $startDate = $('#startDate').val();
+  var $endDate = $('#endDate').val();
+  var $magnitude = $('#magnitude').val();
+  var string = 'https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime='+ $startDate + '&endtime=' + $endDate + '&minmagnitude=' + $magnitude;
+
+  var $xhr = $.getJSON(string);
+
 
    $xhr.done(function(data) {
-     console.log(data);
-     for (var i = 0; i < data.features.length; i++) {
-       var obj = {};
-       obj.magnitude = data.features[i].properties.mag;
-       obj.location = data.features[i].properties.place;
-       obj.time = (new Date(data.features[i].properties.time));
-       obj.latitude = data.features[i].geometry.coordinates[1];
-       obj.longitude = data.features[i].geometry.coordinates[0];
-       dataArr.push(obj);
-     }
-       for (var x = 0; x < dataArr.length; x++) {
-         var latLng = new google.maps.LatLng(dataArr[x].latitude, dataArr[x].longitude);
-         console.log(latLng);
+     $.each(data.features, function(){
+         var contentString = '';
+         var $element = $('<div>');
+         var $time = new Date(this.properties.time);
+         console.log($time);
+         var $mag = this.properties.mag;
+         console.log($mag);
+         var $place = this.properties.place;
+         console.log($place);
+         contentString = '<strong>LOCATION:</strong> ' + this.properties.place.toString() + '<br>'  + ' <strong>MAGNITUDE:</strong> ' + this.properties.mag.toString() + '<br>' + ' <strong>TIME:</strong> ' + new Date(this.properties.time);
+         var $lat = this.geometry.coordinates[1];
+         var $long = this.geometry.coordinates[0];
+         var $placetag = $('<p>');
+         $placetag.append($('<span>').html('Location: '));
+         $placetag.append($('<span>').html($place));
+         $element.append($placetag);
+         var $timetag = $('<p>');
+         $timetag.append($('<span>').html('Time: '));
+         $timetag.append($('<span>').html($time));
+         $element.append($timetag);
+         var $magtag = $('<p>');
+         $magtag.append($('<span>').html('Magnitude: '));
+         $magtag.append($('<span>').html($mag));
+         $element.append($magtag);
+         var $coords = $('<p>');
+         $coords.append($('<span>').html('Lattitude: '));
+         $coords.append($('<span>').html($lat));
+         $coords.append($('<span>').html('Longitude: '));
+         $coords.append($('<span>').html($long));
+         $element.append($coords);
+         $body.append($element);
+
+
+         var latLng = new google.maps.LatLng($lat,$long);
+         var infowindow = new google.maps.InfoWindow({
+         content: contentString,
+         maxWidth: 200
+         });
          var marker = new google.maps.Marker({
            position: latLng,
-           map: map
+           map: map,
+           animation: google.maps.Animation.DROP
          });
-       }
-       console.log(dataArr);
+         marker.addListener('click', function() {
+         infowindow.open(map, marker);
+         });
+         console.log(map.data);
+       });
      });
    $xhr.fail(function(err) {
      console.log(err);
    });
 
- });
+ }
+ $('#userInput').on('submit', getData);
+});
