215047bde39c9123cdd748796c3e843aa03af55b
Jeff Kerpan <jkerpan@gmail.com>
2017-05-11 21:37:47 -0600

    Slight Tweaks

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-//Shaking a picture for a set amount of time when the page loads and then hiding that picture and showing the main content of the page.
-var $shakeSlow = $('.shake-hard');
+//Shaking a picture for a set amount of time when the page loads, then hiding that picture and then showing the main content of the page
+var $shakeHard = $('.shake-hard');
 var $mainContent = $('#mainContent');
 var timeout = setTimeout(function() {
-  $shakeSlow.addClass('hideEarthquake');
+  $shakeHard.addClass('hideEarthquake');
   $mainContent.removeClass('hideThings');
 }, 3000);
 
 $(document).ready(function() {
-  $(".button-collapse").sideNav();
+  $('.button-collapse').sideNav();
 
 //Initialize the Google Map inside of an IIFE so that it loads before everything else
   var map;
@@ -24,11 +24,10 @@ $(document).ready(function() {
       fullscreenControl: true,
     });
 
-    //Resize Google Maps mostly correct when browser window is shrunk down.  Was having issues with the Zoom buttons disapearing when the browser was small, mobile sized.
-    google.maps.event.addDomListener(window, "resize", function() {
-      // console.log('resize');
+    //Resize Google Maps mostly correct when browser window is shrunk down.  Was having issues with the Zoom buttons disapearing when the browser was small, mobile sized
+    google.maps.event.addDomListener(window, 'resize', function() {
       var center = map.getCenter();
-      google.maps.event.trigger(map, "resize");
+      google.maps.event.trigger(map, 'resize');
       map.setCenter(center);
     });
   })();
@@ -36,7 +35,7 @@ $(document).ready(function() {
 //Appened things to the body quickly
 var $body = $('body');
 
-  //When the form/s are submitted this is the data.  The JSON call is dependant on all 3 of these forms being filled in and button clicked.
+  //When the form/s are submitted this is the data. The JSON call is dependant on all 3 of these forms being filled in and the button clicked
   function getData(event) {
   event.preventDefault();
 
@@ -44,31 +43,29 @@ var $body = $('body');
   var $endDate = $('#endDate').val();
   var $magnitude = $('#magnitude').val();
 
-  //Once getData function runs it then plugs the variables into the string.
+  //Once getData function runs it then plugs the variables into the string
   var string = 'https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson' + '&starttime=' + $startDate + '&endtime=' + $endDate + '&minmagnitude=' + $magnitude;
 
-  //This is the actual AJAX request to get the data from USGS API.  Passing in the string variable as the argument so that each time the form data changes it'll pull different data from USGS.
-
+  //This is the actual AJAX request to get the data from the USGS API.  Passing in the string variable as the argument so that each time the form data changes it'll pull different data from USGS
   var $xhr = $.getJSON(string);
 
-  //Once the AJAX request is done, it creates an element div and appends it to the body.
-   $xhr.done(function(data) {
-     $.each(data.features, function(){
-        var $element = $('<div>');
-        $body.append($element);
+  //Once the AJAX request is done, it creates an element div and appends it to the body
+  $xhr.done(function(data) {
+    $.each(data.features, function(){
+      var $element = $('<div>');
+      $body.append($element);
 
-        //Pulling the new Latitudes and Longitudes for each Earthquake
+      //Pulling the new Latitudes and Longitudes for each Earthquake
+      var $lat = this.geometry.coordinates[1];
+      var $long = this.geometry.coordinates[0];
+      var latLng = new google.maps.LatLng($lat,$long);
 
-        var $lat = this.geometry.coordinates[1];
-        var $long = this.geometry.coordinates[0];
-        var latLng = new google.maps.LatLng($lat,$long);
-
-        //Populate Info Windows
-        var infowindow = new google.maps.InfoWindow({
-          content: info_window_content(this.properties),
-          maxWidth: 160,
-          position: latLng
-        });
+      //Populate Info Windows
+      var infowindow = new google.maps.InfoWindow({
+        content: info_window_content(this.properties),
+        maxWidth: 160,
+        position: latLng
+      });
 
         //Setting the style of the Markers to Circles
         map.data.setStyle(function(feature) {
@@ -78,9 +75,9 @@ var $body = $('body');
           };
         });
 
-        //Populates the Circles on the Google Map in the correct position but the scale based on magnitude doesn't work and the Circles are on the map but VERY small
-        let mag = Math.pow(2, this.properties.mag)*10000;
-        let circ = new google.maps.Circle({
+        //Populates the Circles on the Google Map in the correct position based on magnitude. Problems with the circles when you get further away from the equator as the size relation to the magnitude is askew
+        var mag = Math.pow(2, this.properties.mag)*10000;
+        var circ = new google.maps.Circle({
          center:latLng,
          clickable:true,
          fillColor:'red',
@@ -98,19 +95,17 @@ var $body = $('body');
      });
   });
 
-   $xhr.fail(function(err) {
-     console.log(err);
-   });
+  $xhr.fail(function(err) {
+    console.log(err);
+  });
  }
 
- //Triggers the information submission.
-
+ //Triggers the information submission
  $('#userInput').on('submit', getData);
 
 });
 
 //Function to input earthquake info into the Info Windows
-
 function info_window_content(earthquakeData) {
   return '<strong>LOCATION:</strong> ' +
   earthquakeData.place.toString() + '<br>' +
--- troubleshoot.js
+++ troubleshoot.js
@@ -1,92 +1,102 @@
+var $shakeHard = $('.shake-hard');
+var $mainContent = $('#mainContent');
+var timeout = setTimeout(function() {
+  $shakeHard.addClass('hideEarthquake');
+  $mainContent.removeClass('hideThings');
+}, 3000);
+
 $(document).ready(function() {
-  var $body = $('Body');
+  $('.button-collapse').sideNav();
 
-    function getData(event) {
-      event.preventDefault();
+var map;
+(function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 2,
+    center: new google.maps.LatLng(-20, 175),
+    mapTypeId: 'terrain',
+    zoomControl: true,
+    mapTypeControl: true,
+    scaleControl: true,
+    streetViewControl: false,
+    rotateControl: false,
+    fullscreenControl: true,
+  });
 
-      var $startDate = $('#startDate').val();
-      var $endDate = $('#endDate').val();
-      var $magnitude = $('#magnitude').val();
+  google.maps.event.addDomListener(window, 'resize',
+  function() {
+    var center = map.getCenter();
+    google.maps.event.trigger(map, 'resize');
+    map.setCenter(center);
+  });
+})();
 
-      var string = 'http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson' + '&starttime=' + $startDate + '&endtime=' + $endDate + '&minmagnitude=' + $magnitude;
+var $body = $('body');
 
-      var $xrh = $.getJSON(string);
+function getData(event) {
+  event.preventDefault();
 
-      $xhr.done(function(data) {
-        $.each(data.features, function() {
-          var $element = $('<div>');
-          $body.append($element);
+  var $startDate = $('#startDate').val();
+  var $endDate = $('#endDate').val();
+  var $magnitude = $('#magnitude').val();
 
-          var $lat = this.geometry.coordinates[1];
-          var $long = this.geometry.coordinates[0];
-          var latLng = new google.maps.LatLng($lat, $long);
+  var string = 'https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson' + '$starttime=' + $stateDate + '&endtime=' + $endDate + '&minmagnitude=' + $magnitude;
 
-          var infowindow = new google.maps.InfoWindow({
-            context: info_window_content(this.properties),
-            madWidth: 200
-          });
+  var $xhr = $.getJSON(string);
 
-          map.data.setStyle(function(feature) {
-            var magnitude = feature.getProperty('mag');
-            return {
-              icon: getCircle(magnitude)
-            };
-          });
-          console.log(map.data);
+  $xhr.done(function(data) {
+    $.each(data.features, function() {
+      var $element = $('<div>');
+      $body.append($element);
 
-<<<<<<< HEAD
-          var marker = new google.maps.Marker({
-            position: latLng,
-            map: map,
-            animation: google.maps.Animation.DROP
-          });
-          marker.addListener('click', function() {
-            infowindow.open(map, marker);
-          });
-        });
+      var $lat = this.geometry.coordinates[1];
+      var $long = this.geometry.coordinates[0];
+      var latLng = new google.maps.LatLng($lat, $long);
+
+      var infowindow = new google.maps.InfoWindow( {
+        content: info_window_content(this.properties),
+        maxWidth: 160,
+        postion: latLng
       });
-=======
-          var circ = new google.maps.Circle({
-            center:latLng,
-            clickable:true,
-            fillColor:'red',
-            fillOpacity:0.3,
-            map:map,
-            radius:1000,
-           //  scale: 100,
-            strokeColor:'red',
-            strokeOpacity:0.3
-          });
 
-          // var marker = new google.maps.Marker({
-          //   position: latLng,
-          //   map: map,
-          //   animation: google.maps.Animation.DROP
-          // });
-          // marker.addListener('click', function() {
-          //   infowindow.open(map, marker);
-            console.log(circ);
-            circ.addListener('click', function() {
-            infowindow.open(map, circ);
-            });
-          });
-        });
-      
->>>>>>> Circles
-
-      $xhr.fail(function(err) {
-        console.log(err);
+      map.data.setStyle(function(feature) {
+        var magnitude = feature.getProperty('$magnitude');
+        return {
+          icon: getCircle(magnitude)
+        };
       });
-    }
 
-    $('#userInput').on('submit', getData);
+      var mag = Math.pow(2, this.properties.mag)*10000;
+      var circ = new google.maps.Circle( {
+        center: latLng,
+        clickable: true,
+        fillColor: 'red',
+        fillOpacity: 0.3,
+        map: map,
+        radius: mag,
+        strokeColor: 'white',
+        strokeWeight: 0.7
+      });
+
+      circ.addListener('click', function() {
+        infowindow.open(map, circ);
+      });
+    });
+  });
+
+  $xhr.fail(function(err) {
+    console.log(err);
+  });
+}
+
+$('#userInput').on('submit', getData);
+
 });
 
-function info_window_content(props) {
+function info_window_content(earthquakeData) {
   return '<strong>LOCATION:</strong> ' +
-  props.place.toString() + '<br>' +
-  ' <strong>MAGNITUDE:</strong> ' +
-  props.mag.toString() + '<br>' +
-  ' <strong>TIME:</strong> ' +
-  new Date(props.time);
+  earthquakeData.place.toString() + '<br>' +
+  ' <strong>MAGNITUDE:</strong>' +
+  earthquakeData.mag.toString() + '<br>' +
+  ' <strong>DATE:TIME:</strong> ' +
+  new Date(earthquakeData.time);
 }
